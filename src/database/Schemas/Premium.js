6b6b789721520a764aa275e39dcf7520c9077d18
Miraç <88139688+westydev@users.noreply.github.com>
2023-01-30 11:49:25 +0400

    Delete src/database/Schemas directory

--- src/database/Schemas/Premium.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const Premium = new Schema({
-    userID: String,
-    startDate: Date,
-    finishData: Date
-});
-
-module.exports = model('Premium', Premium);
--- src/database/Schemas/Whitelists.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const Premiums = new Schema({
-    users: Array
-});
-
-module.exports = model('Premiums', Premiums);
