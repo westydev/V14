a36436d11bef5f8602026ea2ddb7018f93dd3534
Miraç <88139688+westydev@users.noreply.github.com>
2023-06-08 20:14:15 +0400

    Delete src directory

--- src/database/mongooseConnector.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const { DATABASE } = require("../Settings/Config")
-
-async function connectMongo() {
-    mongoose.connect(DATABASE.mongooseConnection).then(x => console.log("MongoDB bağlantısı kuruldu!")).catch(err => console.log(err));
-}
-
-module.exports = { connectMongo }
